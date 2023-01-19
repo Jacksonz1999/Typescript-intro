e2d545480cb0e38f19863fcc82d80327841f6fa6
Jackson Zhou <jianchenfutbol@gmail.com>
2023-01-19 20:09:09 +0100

    midudev-youtube

---
+++ Typescript Intro/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>
+        Proponer tipos a JS
+        Superset: Quiere decir que todo el codigo Typesript es compatible con Javascript</p>
+    </>
+  )
+}
+
+export default App
